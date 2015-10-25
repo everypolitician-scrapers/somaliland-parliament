e60deefdd6d99ccecaa37e8ec92c6ab1f3c8dd3d
Tony Bowden <tony@tmtm.com>
2015-10-25 08:42:54 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,37 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('.article-content table tr').drop(1).each do |tr|
+    next if tr.text.tidy.empty?
+    td = tr.css('td')
+    data = { 
+      id: td[0].text.tidy,
+      name: td[1].text.tidy.sub('Hon. ', ''),
+      area: td[2].text.tidy,
+      term: 2005,
+      source: url.to_s,
+    }
+    ScraperWiki.save_sqlite([:id, :name, :term], data)
+  end
+end
+
+scrape_list('http://somalilandparliament.net/index.php/members-of-the-hor/65-members-by-constituency-a-party/56-members-of-the-parliament')
